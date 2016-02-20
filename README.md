d639975f42eb110ca8a736d49096711d816343f2
josetya <jonisetyawan@gmail.com>
2016-02-20 14:44:08 +0700

    learn git yoo

---
+++ README.md
@@ -0,0 +1,2 @@
+# coderdojoidtraining
+# learngit
---
+++ def_calculate_area.py
@@ -0,0 +1,8 @@
+def calculate_triangle (height,base):
+    return height * base /2
+result1 = calculate_triangle(10,2)
+result2 = calculate_triangle (30,5)
+result3 = calculate_triangle(15,2)
+print "result 1 = ", result1
+print "result 2 = ", result2
+print "result 3 = ", result3
