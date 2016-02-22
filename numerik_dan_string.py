3267f062b8aa4e117059c6fdea02944183b1e934
josetya <jonisetyawan@gmail.com>
2016-02-22 11:16:02 +0700

    numerik dan string

---
+++ numerik_dan_string.py
@@ -0,0 +1,24 @@
+# numerik
+
+print "numerik"
+
+jumlah_mata = 2
+pi = 3.14
+lobang_celana = 3
+
+print "jumlah mata = ",(jumlah_mata)
+print "pi = ",(pi)
+print "lubang celana = ",(lobang_celana)
+
+# string
+print "String"
+
+nama = 'Joni setyawan'
+istri = 'Solikhah'
+anak_pertama = 'Rafidh Fatikha Wadana'
+anak_kedua = 'Muhammad Alif Syaiful Islam Makarim'
+
+print "Nama = ", (nama)
+print "Istri = ", (istri)
+print "Anak Pertama = ", (anak_pertama)
+print "Anak Kedua = ", (anak_kedua)
