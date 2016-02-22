fc99cca2b6b0a2a4b5187f7ad5d4f7ea5be89754
josetya <jonisetyawan@gmail.com>
2016-02-22 21:16:37 +0700

    perulangan dengan while

---
+++ perulangan_dengan_while.py
@@ -0,0 +1,11 @@
+#perulangan dengan while
+
+lawan_kata = {}
+lawan_kata ['panjang']= 'pendek'
+lawan_kata ['besar']= 'kecil'
+lawan_kata ['tinggi']= 'rendah'
+lawan_kata ['siang']= 'malam'
+
+lawan_kata = True
+while lawan_kata:
+    print ('perulangan dengan while')
