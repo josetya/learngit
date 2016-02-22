e032ee9c3b011c0b9531230d165ad56e8c961e10
josetya <jonisetyawan@gmail.com>
2016-02-22 20:36:32 +0700

    Percabangan Logika Dengan if

---
+++ percabangan_logika_if.py
@@ -0,0 +1,15 @@
+#percabangan logika dengan if
+
+print "Percabangan Logika Dengan if"
+
+negara = "Indonesia"
+negara = "Jepang"
+if negara == "Indonesia":
+    print "Indonesia ",('Zamrud Katulistiwa')
+elif negara == "China":
+    print "China ",('Tirai Bambu')
+elif negara == "Australia":
+    print "Australia",('kangooro')
+
+else:
+    print "Hmmm, Sek tak pikire dulu"
