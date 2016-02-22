dce3712058636f3ba297c63a2ad08163700e6f39
josetya <jonisetyawan@gmail.com>
2016-02-22 19:32:20 +0700

    array and dictionary

---
+++ array_dan_dictionary.py
@@ -0,0 +1,27 @@
+#array
+
+print "belajar array"
+
+siswa = []
+siswa.append('joni')
+siswa.append('dzikers')
+siswa.append('abdilahrf')
+siswa.append('aryandiw')
+siswa.append('egi361')
+siswa.append('eko')
+siswa.append('fuadh')
+print (siswa[3])
+print (siswa[5])
+
+#dictionary
+
+print "belajar dictionary"
+
+lawan_kata = {}
+lawan_kata ['panjang']= 'pendek'
+lawan_kata ['besar']= 'kecil'
+lawan_kata ['tinggi']= 'rendah'
+lawan_kata ['siang']= 'malam'
+
+print (lawan_kata ['besar'])
+print (lawan_kata ['siang'])
