799b7baf1c9ceb1ff954ef8583bf94f4d6b67fcf
josetya <jonisetyawan@gmail.com>
2016-02-22 21:02:34 +0700

    perulangan dengan for

---
+++ perulangan.py
@@ -0,0 +1,29 @@
+#perulangan dengan for
+
+print "perulangan dengan for"
+
+for a in range(0,12):
+    print "perulangan ke ",a
+
+#perulangan terhadap suatu list
+
+print "perulangan terhadap suatu list"
+
+siswa = []
+siswa.append('joni')
+siswa.append('dzikers')
+siswa.append('abdilahrf')
+siswa.append('aryandiw')
+siswa.append('egi361')
+siswa.append('eko')
+siswa.append('fuadh')
+for s in siswa:
+    print(s)
+
+lawan_kata = {}
+lawan_kata ['panjang']= 'pendek'
+lawan_kata ['besar']= 'kecil'
+lawan_kata ['tinggi']= 'rendah'
+lawan_kata ['siang']= 'malam'
+for l in lawan_kata:
+    print l
